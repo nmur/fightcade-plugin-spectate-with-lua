60f9b073f21635d4f9ba8f2ee56f48c0ee7296fd
Nicholas Murray <nickmurray91@gmail.com>
2025-09-24 00:35:38 +1000

    Add files via upload

---
+++ spectateWithLua.js
@@ -0,0 +1,37 @@
+const { exec } = require('child_process');
+const config = require('./config.json');
+
+module.exports = (FCADE) => { runPlugin(FCADE) };
+
+const fbneoPath = "C:\\Users\\nmur\\Documents\\Fightcade\\emulator\\fbneo\\fcadefbneo.exe";
+const luaPath = "--lua C:\\Users\\nmur\\Documents\\Fightcade\\emulator\\fbneo\\fbneo-training-mode\\3rd_spectator.lua";
+
+const runPlugin = (FCADE) => {
+    // Override the Playing context menu 
+    FCADE.$options.components["Channel"].components.UsersList.methods.showMatchContextMenu = function(e, t, n) {
+        this.channel.spectators && (this.contextMenuData = {
+            user: e.player1,
+            position: t,
+            limitRect: n,
+            callback: () => {
+                spectateWithLua(this.channel.gameid, e.quarkId, e.port)
+            },
+            items: [{
+                id: "spectate",
+                text: "Spectate With LUA"
+            }]
+        },
+        this.$emit("show-context-menu", this.contextMenuData))
+    }
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "F11") {
+            exec(commandStr, (error, stdout, stderr) => {});
+        }
+    });
+}
+
+const spectateWithLua = (gameId, quarkId, port) => {
+    const commandStr = fbneoPath + " " + luaPath + " " + "quark:stream," + gameId + "," + quarkId + ".2," + port;
+    exec(commandStr, (error, stdout, stderr) => {});
+}
